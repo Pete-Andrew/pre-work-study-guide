0e11e174379d21bf46c5eabc991780e923ed2cbb
Pete Andrew <112977421+Pete-Andrew@users.noreply.github.com>
2022-10-28 16:53:40 +0100

    added more JS

--- prework-study-guide/script.js
+++ prework-study-guide/script.js
@@ -1,19 +1,30 @@
-var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+var Topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+var RandomTopic = Topics[Math.floor(Math.random() * Topics.length)];
 
-for (var x = 0; x < topics.length; x++) {
-console.log (topics[x]);    
+function ListTopics() {
+
+    for (var x = 0; x < Topics.length; x++) 
+    {
+        console.log (Topics[x]);    
+    }
+} //prints out the list of var Topics
+
+function SelectTopic() {
+if (RandomTopic === 'HTML') 
+    {
+    console.log("Let's study HTML!");
+    } else if (RandomTopic === 'CSS') {
+    console.log("Let's study CSS!");
+    } else if (RandomTopic === 'Git') {
+    console.log("Let's study Git!");
+    } else if (RandomTopic === 'JavaScript') {
+    console.log("Let's study JavaScript!");
+    } else {
+    console.log('Please try again!');
+    }
 }
 
-/*
-if (topic === 'HTML') {
- console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
- console.log("Let's study CSS!");
-} else if (topic === 'Git') {
- console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
- console.log("Let's study JavaScript!");
-} else {
- console.log('Please try again!');
-}
-*/
+console.log('Here are the topics we learned through prework:')
+ListTopics()
+console.log('Which topic should we study first?');
+SelectTopic();
